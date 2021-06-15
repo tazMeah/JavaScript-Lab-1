94a832648615d5a2376455e272dc9bb4d66b11b3
Montaz Meah <montaz.meah@mrm.com>
2021-06-15 06:10:47 -0400

    Initial commit. Will upload a video of this to YouTube and create a README file which includes the link to the video.

---
+++ script.js
@@ -0,0 +1,40 @@
+let name = "taz";
+let age = 100;
+let birthday = "March 7";
+let detroitGC = true;
+let lifeEvents = [
+  "I was born in Detroit, MI",
+  "I went to Kettering University",
+  "I went Junior Olympics",
+  "I'm a graduate of the front-end bootcamp",
+];
+
+if (detroitGC) {
+  console.log(
+    `My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`
+  );
+} else {
+  console.log(
+    `My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`
+  );
+}
+
+for (let i = 0; i < lifeEvents.length; i++) {
+  console.log(lifeEvents[i]);
+}
+
+let counter = 0;
+
+while (true) {
+  let randomNumber = Math.ceil(Math.random() * 10);
+  if (randomNumber !== 5) {
+    counter++;
+    console.log(`${randomNumber} !== 5`);
+  } else {
+    counter++;
+    console.log(
+      `5 === 5. It took ${counter} iterations to randomly generate the number 5.`
+    );
+    break;
+  }
+}
